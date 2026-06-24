a3477eba18c9f831c267ffdc9e92597a98a406de
beseo <tjquddml@gmail.com>
2026-06-24 12:59:06 +0900

    Initial: sample container app for ACA test

---
+++ aca-test-app/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const os = require('os');
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    message: 'Hello from Azure Container Apps test!',
+    hostname: os.hostname(),
+    time: new Date().toISOString()
+  }));
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
